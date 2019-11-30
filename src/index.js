6d8985321b6ccc2049a53227ff1b9de07c972612
Michal Stepien <michal.stepien36@gmail.com>
2019-11-30 14:57:09 +0100

    jezus

--- src/index.js
+++ src/index.js
@@ -1,23 +1,34 @@
 import "./styles/main.scss";
-import {addInputListener} from './app/search-controller'
+import { addInputListener } from "./app/search-controller";
 // import * as searchModel from './app/search-model'
 
-addInputListener()
+addInputListener();
 // fetchData()
 const placeholder = () => {
-    const book = [{title: 'Harry Potter',
-        author_name: 'J. K. Rowling',
-        publish_date: "2003 November",
-        wydawnictwo: 'ABC',
-        isbn: 'dfadf213987'
-}];
-const cover = document.getElementById('okladka')
-cover.setAttribute('src', 'https://d827xgdhgqbnd.cloudfront.net/wp-content/uploads/2016/04/09121712/book-cover-placeholder-188x300.png')
-    
-}
+  const book = [
+    {
+      title: "Harry Potter",
+      author_name: "J. K. Rowling",
+      publish_date: "2003 November",
+      ilość_stron: "220",
+      wydawnictwo: "ABC",
+      isbn: "dfadf213987"
+    }
+  ];
+  const cover = document.getElementById("okladka-img");
+  cover.setAttribute(
+    "src",
+    "https://d827xgdhgqbnd.cloudfront.net/wp-content/uploads/2016/04/09121712/book-cover-placeholder-188x300.png"
+  );
+  document.getElementById("span-tytuł").innerText(book.title);
+  document.getElementById("span-autor").innerText(book.author_name);
+  document.getElementById("span-data").innerText(book.publish_date);
+  document.getElementById("span-ilość").innerText(book.ilość_stron);
+  document.getElementById("span-wydawnictwo").innerText(book.wydawnictwo);
+  document.getElementById("span-ISBN").innerText(book.isbn);
+};
 
-
-placeholder()
+placeholder();
 
 // const endpoint = "https://api.punkapi.com/v2/beers";
 // const beers = [];
