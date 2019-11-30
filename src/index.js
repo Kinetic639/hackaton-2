24fb41d1adfd953424fff01b8ba36f41537bbd89
Michal Stepien <michal.stepien36@gmail.com>
2019-11-30 10:20:21 +0100

    drugi commit

---
+++ src/index.js
@@ -0,0 +1,49 @@
+import "./styles/main.scss";
+
+const endpoint = "https://api.punkapi.com/v2/beers";
+const beers = [];
+const searchInput = document.querySelector(".search");
+const suggestions = document.querySelector(".suggestions");
+
+const submitSearch = e => {
+  //   e.preventDefault();
+  fetch(endpoint)
+    .then(response => response.json())
+    .then(data => beers.push(...data));
+
+  console.log(beers);
+};
+
+function FindMatches(wordToMatch, beers) {
+  return beers.filter(beer => {
+    const regex = new RegExp(wordToMatch, "gi");
+    return beer.name.match(regex);
+  });
+}
+
+function displayMatches() {
+  submitSearch();
+  const matchArray = FindMatches(this.value, beers);
+  console.log(matchArray);
+  const html = matchArray
+    .map(beer => {
+      return `
+		<li>
+		<span> ${beer.name}</span>
+		</li>
+		`;
+    })
+    .join();
+  suggestions.innerHTML = html;
+  //   console.log(this.value);
+}
+
+searchInput.addEventListener("change", displayMatches);
+searchInput.addEventListener("keyup", displayMatches);
+
+const addSubmitListener = () => {
+  const buttonSubmit = document.getElementById("btn-submit");
+  buttonSubmit.addEventListener("click", submitSearch);
+};
+
+addSubmitListener();
---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader",
+            options: { minimize: true }
+          }
+        ]
+      },
+      {
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"]
+      },
+      {
+        test: /\.svg$/,
+        loader: "svg-inline-loader"
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+          },
+        ],
+      },
+    ]
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./src/index.html",
+      filename: "./index.html"
+    })
+  ]
+};
